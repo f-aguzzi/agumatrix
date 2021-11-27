f4fd3c66e09a04db4563a441dc1a20638898270c
Federico Aguzzi <federico.aguzzi@icloud.com>
2021-11-27 11:13:32 +0100

    Added the deter function (Laplacian expansion algorithm)

--- src/lib.rs
+++ src/lib.rs
@@ -50,13 +50,34 @@ impl Matrix {
 				}
 
 				if y >= col {
-					col_1 += 1
+					col_1 += 1;
 				}
 
-				mat.set(row_1, col_1, self.get(x, y))
+				mat.set(x, y, self.get(row_1, col_1))
 			}
 		}
 
 		mat
+	}
+
+
+	// Laplacian expansion algorithm (recursive)
+	pub fn deter(&mut self) ->f64 {
+		let mut deter: f64 = 0.0;
+
+		// basic case
+		if self.size == 2 {
+			deter += (self.get(1,2) * self.get(2,1) - self.get(1,1) * self.get(2,2))
+		}
+
+		// recursion
+		else {
+			for x in 1..=self.size {
+				deter += pow(-1.00, (x+1) as usize) * self.get(1,x) * self.compl(1,x).deter()
+			}
+		}
+
+		deter
+
 	}
 }
